1477cdb7474731c6141a2bcc49919d048a32840b
HecAnunah <pkmaster2@gmail.com>
2025-03-08 16:32:17 +0300

    'mini adds'

--- ChekiO/1 island String and Integers/Високостный год.py
+++ ChekiO/1 island String and Integers/Високостный год.py
@@ -1,6 +1,5 @@
 def is_leap_year(year: int) -> bool:
-    # your code here
-    return False
+    return True if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0 else False
 
 
 print("Example:")
